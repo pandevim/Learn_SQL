f4024c17c527d0ac3aedffce7739174e463a5232
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2017-12-14 04:06:33 +0530

    Create 14_12_17.sql

---
+++ Aniruddha/14_12_17.sql
@@ -0,0 +1,6 @@
+/* 
+   All queries are done on the database objects called 'tables'.
+   These tables are made up of 'fields'.
+*/
+
+SELECT * FROM Customers --This SQL statement selects all the records from the table name 'Customers'.
