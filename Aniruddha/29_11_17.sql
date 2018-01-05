bbe775d65082538dde0869103ece84a0ec67acf5
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-05 21:39:00 +0530

    Update 29_11_17.sql

--- Aniruddha/29_11_17.sql
+++ Aniruddha/29_11_17.sql
@@ -1,6 +1,7 @@
 /* 
    SQL is a standard for languages, but it is not a language itself!
    Oracle, Microsoft and MySQL are some of the examples of languages of SQL.
+   like PL/SQL, JET SQL, T -SQL and respectively.
 */
 
 --A primary key is a field in the table that uniquely identifies the table records which can't be NULL.
