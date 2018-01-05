c2cf8adc6425aa3992f15a3ab115a87c71920724
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-06 00:03:09 +0530

    Update 05_01_18.sql

--- Aniruddha/05_01_18.sql
+++ Aniruddha/05_01_18.sql
@@ -5,6 +5,5 @@ WHERE Country='Mexico';
 
 /* 
    "WHERE" clause will filter the database where display countires are only on "Mexico".
-   "WHERE" works like a "if" condition, it should be satified to complete a task.
 */
 
