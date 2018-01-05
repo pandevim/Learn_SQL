eee0e84ab8ac1619b825966bd297029d5991f7a1
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-05 22:39:50 +0530

    Update 05_01_18.sql

--- Aniruddha/05_01_18.sql
+++ Aniruddha/05_01_18.sql
@@ -1,8 +1,10 @@
 SELECT
    *
 FROM Customers
-WHERE Country="Mexico";
+WHERE Country='Mexico';
 
 /* 
-   'WHERE' clause will filter the database where display countires are only on "Mexico".
+   "WHERE" clause will filter the database where display countires are only on "Mexico".
+   "WHERE" works like a "if" condition, it should be satified to complete a task.
 */
+
