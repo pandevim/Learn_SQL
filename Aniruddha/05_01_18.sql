f5c899f9ac63dadd68dc54b96ed5ff58d4b02232
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-06 18:58:58 +0530

    Update 05_01_18.sql

--- Aniruddha/05_01_18.sql
+++ Aniruddha/05_01_18.sql
@@ -7,3 +7,22 @@ WHERE Country='Mexico';
    "WHERE" clause will filter the database where display countires are only on "Mexico".
 */
 
+/* 
+   Operators can also be used with "WHERE" clause like this in below.
+*/
+
+SELECT
+   *
+FROM Customers
+WHERE CustomerID=1;
+
+SELECT
+   *
+FROM Customers
+WHERE Country<'Germany';
+
+/* 
+   This will show the countries which have their first alphapbet lower, than the first alphabet of "Germany" that is "G",
+   in alpabetical order.
+*/
+
