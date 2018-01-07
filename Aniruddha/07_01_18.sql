4d35d577492c1f55b491175fbf624fc8f223537f
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-07 17:35:10 +0530

    Update 07_01_18.sql

--- Aniruddha/07_01_18.sql
+++ Aniruddha/07_01_18.sql
@@ -2,4 +2,32 @@
    Wildcards are used with "LIKE" operator:-
    _ : one character
    % : more than one character
+   []: set and ranges of character to match
+*/
+
+SELECT
+   *
+FROM Customers
+WHERE Country LIKE 'G%';
+
+/* 
+   The above query will find any value that start with G.
+*/
+
+SELECT
+   *
+FROM Customers
+WHERE Country LIKE '%G';
+
+/* 
+   The above query will find any value that end with G.
+*/
+
+SELECT
+   *
+FROM Customers
+WHERE Country LIKE '_g%';
+
+/* 
+   The above query will find any value whose second charater starts with G.
 */
