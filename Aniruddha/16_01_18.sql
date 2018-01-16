5673ff9c57ed2f0500d22202a0816dc5533738fc
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-16 22:01:00 +0530

    Update 16_01_18.sql

--- Aniruddha/16_01_18.sql
+++ Aniruddha/16_01_18.sql
@@ -1,10 +1,20 @@
 /* 
    "IN" operator can be used instead of using multiple "OR".
+   It is also called a  "IN" statement filter.
 */
 
 SELECT
    *
 FROM Customers
-WHERE Country='Germany'
-OR	  Country='France'
-OR	  Country='UK';
+WHERE Country='Germany' OR Country='France' OR Country='UK';
+
+/* 
+   The above query will give the table with only countries germany, france and  uk.
+   But this same result can be obtained by using "IN" filter.
+   Like this below.
+*/
+
+SELECT
+   *
+FROM Customers
+WHERE Country IN ('Germany','France','UK');
