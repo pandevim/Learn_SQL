0f3bcbfcc8e01a3264151029511ddade4fc0e2bf
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-20 23:14:54 +0530

    Update 20_01_18_II.sql

--- Aniruddha/20_01_18_II.sql
+++ Aniruddha/20_01_18_II.sql
@@ -24,8 +24,22 @@ WHERE Price BETWEEN 10 AND 20 AND NOT CategoryID IN (1,2,3);
 
 /* 
    "WHERE" clause can also be used to sort alphabets.
-   Along with "ORDER BY" clause explained earlier.
 */
 
+SELECT
+   *
+FROM Products
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni';
 
+/* 
+   The above query will give all the "ProductName" between "C" and "M"
+   as they are the first alphabet of selected "ProductName".
+   To have a proper arrangement we can sort these according to alphabetical
+   order by using "ORDER BY" clause in asceding order by defalut as shown below.
+*/
 
+SELECT
+   *
+FROM Products
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni';
+ORDER BY ProductName;
