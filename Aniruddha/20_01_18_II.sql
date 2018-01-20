2980e3e31c782afc12e5cb5cf1e278aaeb9ebe4e
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-20 15:26:23 +0530

    Create 20_01_18_II.sql

---
+++ Aniruddha/20_01_18_II.sql
@@ -0,0 +1,23 @@
+/* 
+   "BETWEEN" operator is used to specify range for some query.
+*/
+
+SELECT
+   *
+FROM Products
+WHERE Price BETWEEN 10 AND 20;
+
+/* 
+   The above operations will give a table with price greater than "10" and less than "20".
+   Operators like "NOT" and "IN" can also be used like this below. 
+*/
+
+SELECT
+   *
+FROM Products
+WHERE Price NOT BETWEEN 10 AND 20;
+
+SELECT
+   *
+FROM Products
+WHERE Price BETWEEN 10 AND 20 AND NOT CategoryID IN (1,2,3);
