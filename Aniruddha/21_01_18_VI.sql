707edc503872cdda7e284502b188c80ca671bc5b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-21 22:58:26 +0530

    Update 21_01_18_VI.sql

--- Aniruddha/21_01_18_VI.sql
+++ Aniruddha/21_01_18_VI.sql
@@ -1,1 +1,7 @@
-
+/* 
+   "AS" clause ofter abbreviated as "Aliases" are used.
+   Dictionary meaning: False name to conceal identity.
+   They are used to give table/column a tempory name
+   as per one's convenience, which exists only
+   for the duration of the query. 
+*/
