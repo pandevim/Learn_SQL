5fb1493bbef2516500e005faf9cb5cb26bdd93ff
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-21 18:26:27 +0530

    Create 21_01_18_IV.sql

---
+++ Aniruddha/21_01_18_IV.sql
@@ -0,0 +1,20 @@
+/* 
+   The "DELETE" statement is used to delete existing records.
+   Lets us make row by using "INSERT INTO" clause.
+   Then delete it!
+*/
+
+INSERT INTO
+   Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Aniruddha Pandey','Tody','LPU','Jalandar','144411','India');
+
+/* 
+   The above query will create a row at "CustomerID=92"!
+*/
+
+DELETE FROM Customers
+WHERE CustomerID=92;
+
+/* 
+   The above query will delete that extra created row in onec.
+*/
