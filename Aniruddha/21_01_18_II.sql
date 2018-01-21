4051381d13a2448bdd5a4dec937c13dd37343f73
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-21 17:54:53 +0530

    Update 21_01_18_II.sql

--- Aniruddha/21_01_18_II.sql
+++ Aniruddha/21_01_18_II.sql
@@ -1,1 +1,12 @@
+/* 
+   A field with "NULL" value doesn't mean empty.
+   I just has been left blank during record creation.
+*/
 
+SELECT
+   CustomerName,
+   ContactName,
+   Address
+FROM Customers
+WHERE Address IS NULL; --"IS NOT NULL" can also be used to extract table which doesn't have any "NULL" value.
+  
