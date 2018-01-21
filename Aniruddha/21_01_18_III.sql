39e2556ede837a1a0074d00b4c1ea149fee65225
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-21 18:04:24 +0530

    Create 21_01_18_III.sql

---
+++ Aniruddha/21_01_18_III.sql
@@ -0,0 +1,18 @@
+/* 
+   The "UPDATE" statement is used to modify the existing record.
+*/
+
+UPDATE Customers
+SET ContactName='Aniruddha Pandey',City='Indore';
+
+/* 
+   The below query will update all the columns
+   in the table "Customers".
+   We can also specify the rows
+   in which updation is required using "WHERE" clause.
+   Like mention below.
+*/
+
+UPDATE Customers
+SET ContactName='Aniruddha Pandey',City='Indore'
+WHERE CustomerID = 1;
