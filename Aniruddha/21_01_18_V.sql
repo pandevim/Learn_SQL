4bea92e024601cc4bd7957d4de81d4381b9ee938
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-21 20:15:28 +0530

    Update 21_01_18_V.sql

--- Aniruddha/21_01_18_V.sql
+++ Aniruddha/21_01_18_V.sql
@@ -1,3 +1,13 @@
 /* 
-   "ROWNUM" claus used in oracle sql versions.
+   "ROWNUM" clause used in oracle sql versions.
+   Is used to fetch limited number/percentage of records from top. 
+*/
+
+SELECT
+   *
+FROM Customers
+WHERE ROWNUM<=3;
+
+/* 
+   The above query will return top 3 rows. 
 */
