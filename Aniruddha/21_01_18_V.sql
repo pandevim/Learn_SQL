6f3960918974f8976648028f2eb939e1a8ba840d
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-21 20:21:58 +0530

    Update 21_01_18_V.sql

--- Aniruddha/21_01_18_V.sql
+++ Aniruddha/21_01_18_V.sql
@@ -9,5 +9,12 @@ FROM Customers
 WHERE ROWNUM<=3;
 
 /* 
-   The above query will return top 3 rows. 
+   The above query will return top 3 rows.
+   The below query will shop the top 3 rows with
+   country as "Germany".
 */
+
+SELECT
+   *
+FROM Customers
+WHERE ROWNUM<=3 AND Country='Germany';
