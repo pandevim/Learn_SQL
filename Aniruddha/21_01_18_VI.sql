68f7c270c45341d1187d57a056a0fa816c4c93b0
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-22 19:02:38 +0530

    Update 21_01_18_VI.sql

--- Aniruddha/21_01_18_VI.sql
+++ Aniruddha/21_01_18_VI.sql
@@ -1,7 +1,17 @@
 /* 
-   "AS" clause ofter abbreviated as "Aliases" are used.
+   "AS" clause often abbreviated as "Aliases" are used.
    Dictionary meaning: False name to conceal identity.
    They are used to give table/column a tempory name
    as per one's convenience, which exists only
    for the duration of the query. 
+*/
+
+SELECT
+   CustomerID as Id,CustomerName AS Name
+FROM Customers;
+
+/* 
+   This query is for column aliases which will return
+   two column with title "Id" and "Name" aliases with
+   "CustomerID" and "CustomerName" respectively.
 */
