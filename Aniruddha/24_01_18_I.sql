d5babb6e0cfa6961fcd97cace9b8c0ee297c7f72
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-24 13:37:08 +0530

    Update 24_01_18_I.sql

--- Aniruddha/24_01_18_I.sql
+++ Aniruddha/24_01_18_I.sql
@@ -1,7 +1,15 @@
-SELECT min(Price) AS [Mininum Price], max(Price) AS [Maximum Price]
+SELECT MIN(Price) AS [Mininum Price],
+       MAX(Price) AS [Maximum Price],
+       COUNT(ProductID) AS [Total ID],
+       AVG(Price) AS [Average Price],
+       SUM(Price) AS [Total Price]
 FROM Products;
 
 /* 
-   The min() and max() fucntion used above will return mininum and maximum
+   The "min()" and "max()" fucntion used above will return mininum and maximum
    numerical/alphabetical value respectively.
+   "count()" function will return the total or number of rows that matches
+   the condition specified.
+   "avg()" function will return the average value of a numerical column.
+   "sum()" function will return the total summation of a numerical column.
 */
