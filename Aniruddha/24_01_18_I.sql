848d11fb77a62752d49e237528b33a4e051a51d2
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-24 13:14:45 +0530

    Create 24_01_18_I.sql

---
+++ Aniruddha/24_01_18_I.sql
@@ -0,0 +1,7 @@
+SELECT min(Price) AS [Mininum Price], max(Price) AS [Maximum Price]
+FROM Products;
+
+/* 
+   The min() and max() fucntion used above will return mininum and maximum
+   numerical/alphabetical value respectively.
+*/
