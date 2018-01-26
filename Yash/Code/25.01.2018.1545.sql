7b6799e6d526cdda83629ec2b05cc5cd77cb58dd
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-26 22:51:47 +0530

    debug typo

--- Yash/Code/25.01.2018.1545.sql
+++ Yash/Code/25.01.2018.1545.sql
@@ -1,14 +1,14 @@
-The SELECT statement
+--The SELECT statement
 SELECT*FROM
 SELECT CustomerID, City FROM Customers; /*This is to select the column from a table. Output: The colums of CustomerID and Customers*/
 
-The SELECT DISTINCT statement
+--The SELECT DISTINCT statement
 SELECT DISTINCT Country FROM Customers; /*In the database there are 91 enteries. Outuput This will tell the distinct countries i.e. list of 21 Countries*/
 
-The COUNT statement
+--The COUNT statement
 SELECT COUNT (DISTINCT Country) FROM Customers/*Output: 21. COUNT tells the numerical value of the property*/
 
-The WHERE statement
+--The WHERE statement
 /*This is used to filter records.*/
 SELECT * FROM Customers
 WHERE Country='Mexico'; /*Output: This displays the records of all the data which contains Country as MEXICO*/
