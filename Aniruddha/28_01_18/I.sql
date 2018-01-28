50efbe9e0b53fb8b8dea4e701c5c66fc60d3f643
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-28 16:29:13 +0530

    Update I.sql

--- Aniruddha/28_01_18/I.sql
+++ Aniruddha/28_01_18/I.sql
@@ -8,11 +8,15 @@ SELECT
 FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 
 /* 
-   "INNER JOIN" return record common to both table.
    The syntax is as follows:-
    
    SELECT
       table.column
    FROM table_1 INNER JOIN table_2 ON table_1.matched_column=table_2.matched_column;
    
+   "INNER JOIN" return record common to both table.
+   The above query will both tables "Orders" and "Customers" joint together
+   with common "CustomerID" column.
+   The "Orders" table will be in left side of the overall display table
+   as it sit the table_1.
 */
