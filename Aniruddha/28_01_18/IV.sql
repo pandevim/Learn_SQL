8bd9b932d727946afb36f5b0aacf04c5ffabb63b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-28 20:20:32 +0530

    Update IV.sql

--- Aniruddha/28_01_18/IV.sql
+++ Aniruddha/28_01_18/IV.sql
@@ -18,3 +18,9 @@ SELECT
    A.City
 FROM Customers A JOIN Customers B ON A.CustomerID != B.CustomerID AND A.City=B.City
 ORDER BY A.City;
+
+/* 
+   The "CROSS JOIN" also know as "Cartesian Join" since do Cartesian Product.
+   Each row in the left_table is paired to the right_table.
+   i.e Produced Result Set = (no. of rows in left_table * no. of rows in right_tables).
+*/
