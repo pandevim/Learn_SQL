abf7f395d47dd72b797ae424baf320904d895ecb
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-28 17:04:06 +0530

    Create II.sql

---
+++ Aniruddha/28_01_18/II.sql
@@ -0,0 +1,6 @@
+/* 
+   The "LEFT JOIN" or "LEFT OUTER JOIN" clause will return
+   records from left table with matching right table records.
+   Basically same as "INNER JOIN" but additionally it will show the rows
+   which were "NULL" or doesn't have any match with the common column.
+*/
