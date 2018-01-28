4b3c7dfd96c1bccf9e1a0853229bcc52f82c9de9
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-28 17:21:06 +0530

    Update III.sql

--- Aniruddha/28_01_18/III.sql
+++ Aniruddha/28_01_18/III.sql
@@ -1,5 +1,5 @@
 /* 
-   The "OUTER JOIN" or "FULL JOIN" clause will return all the content of the both tables
+   The "OUTER JOIN" or "FULL JOIN" or "FULL OUTER JOIN" clause will return all the content of the both tables
    merged together either they are matched or not.
 */
 
