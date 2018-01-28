f51884b052e6fa7953bc129c96a9d82925ab225e
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-28 18:58:37 +0530

    Update IV.sql

--- Aniruddha/28_01_18/IV.sql
+++ Aniruddha/28_01_18/IV.sql
@@ -2,3 +2,20 @@
    The "SELF JOIN" clause will do a regular join, but the table is joined with itself.
 */
 
+SELECT
+   A.CustomerName AS Name1,
+   B.CustomerName AS Name2,
+   A.City
+FROM Customers AS A,Customers AS B
+WHERE A.CustomerID <> B.CustomerID AND A.City=B.City
+ORDER BY A.City;
+
+--OR
+
+SELECT
+   A.CustomerName AS Name1,
+   B.CustomerName AS Name2,
+   A.City
+FROM Customers A,Customers B
+WHERE A.CustomerID != B.CustomerID AND A.City=B.City
+ORDER BY A.City;
