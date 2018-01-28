16127fc507884cb5e0d0eb4724ae6fa0dce8c467
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-28 16:00:51 +0530

    Create I.sql

---
+++ Aniruddha/28_01_18/I.sql
@@ -0,0 +1,8 @@
+/* 
+   The "JOIN" clause is used to combine rows from two or more tables,
+   based on a related column between them.
+*/
+
+SELECT
+   *
+FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
