6078c6b3a169e1ed69e63da3425ebeb6394ca34e
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-28 17:19:32 +0530

    Update III.sql

--- Aniruddha/28_01_18/III.sql
+++ Aniruddha/28_01_18/III.sql
@@ -2,3 +2,7 @@
    The "OUTER JOIN" or "FULL JOIN" clause will return all the content of the both tables
    merged together either they are matched or not.
 */
+
+SELECT
+   *
+FROM Orders FULL JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
