a2499a22fc8c437c1d2c121de43755797d7519ab
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-30 01:09:34 +0530

    Update 30_01_18_I.sql

--- Aniruddha/30_01_18_I.sql
+++ Aniruddha/30_01_18_I.sql
@@ -10,9 +10,12 @@ FROM Customers
 GROUP BY Country;
 
 /* 
-   The "GROUP BY" clause precedes "ORDER BY" clause and will arrange data in ascending order by default.
-   To arrange in descending order "ASC" operator along with "ORDER BY" clause can be used.
-   And since "WHERE" clause can't be used with aggregate functions, "HAVING" clause is used.
+   The "GROUP BY" clause precedes "ORDER BY" clause
+   and will arrange data in ascending order by default.
+   To arrange in descending order "ASC" operator
+   along with "ORDER BY" clause can be used.
+   And since "WHERE" clause can't be used with
+   aggregate functions, "HAVING" clause is used.
    Like this below.
 */
 
@@ -22,3 +25,17 @@ SELECT
 FROM Customers
 GROUP BY Country
 HAVING COUNT(CustomerID)>5 ORDER BY COUNT(CustomerID);
+
+/* 
+   Further more these clauses can be used
+   in addition with other clauses to make a query.
+   Like the below query lists the employees
+   that have registered more than 10 orders.
+*/
+
+SELECT
+   Employees.LastName,
+   COUNT(Orders.OrderID) AS NumberOfOrders
+FROM Orders INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 10;
