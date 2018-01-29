3ea73b83b7a2d8ae63878a646d8328fccf88db7f
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-30 01:02:58 +0530

    Update 30_01_18_I.sql

--- Aniruddha/30_01_18_I.sql
+++ Aniruddha/30_01_18_I.sql
@@ -12,4 +12,13 @@ GROUP BY Country;
 /* 
    The "GROUP BY" clause precedes "ORDER BY" clause and will arrange data in ascending order by default.
    To arrange in descending order "ASC" operator along with "ORDER BY" clause can be used.
+   And since "WHERE" clause can't be used with aggregate functions, "HAVING" clause is used.
+   Like this below.
 */
+
+SELECT
+   COUNT(CustomerID) AS Quantity,
+   Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID)>5 ORDER BY COUNT(CustomerID);
