e7c6735b8ed116edec90cd15a0c7d4b2acc96e05
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-30 01:22:08 +0530

    Create 30_01_18_II.sql

---
+++ Aniruddha/30_01_18_II.sql
@@ -0,0 +1,13 @@
+/* 
+   The "EXISTS" operator checks the existence of a result of a Subquery.
+   Returns true when able to fetch at least one row or else false.
+*/
+
+SELECT
+   SupplierName
+FROM Suppliers
+WHERE EXISTS(
+  SELECT
+     ProductName
+  FROM Products WHERE Products.SupplierID = Suppliers.SupplierID AND Price<20
+);
