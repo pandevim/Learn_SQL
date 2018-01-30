d684b63ec2d829544f579d21de47c2b3114eaa2b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-30 15:13:21 +0530

    Update 30_01_18_III.sql

--- Aniruddha/30_01_18_III.sql
+++ Aniruddha/30_01_18_III.sql
@@ -2,3 +2,19 @@
    "SELECT INTO" statement copies data from one table into a new table.
 */
 
+SELECT
+   *
+INTO Customers_Backup
+FROM Customers;
+
+/* 
+   The above query will create a new table Customers_Backup,
+   and copy all the columns from Customers to this table.
+   If we want to copy this in another database
+   we use the below query.
+*/
+
+SELECT
+   *
+INTO Customers_Backup IN 'Backup.mdb'
+FROM Customers;
