5cd69bdb38225cc78a8db53d43a25e61d35e61e7
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-30 23:16:44 +0530

    Update 30_01_18_I.sql

--- Aniruddha/DataBase/30_01_18_I.sql
+++ Aniruddha/DataBase/30_01_18_I.sql
@@ -1,6 +1,14 @@
 /* 
-   A Database is the main container, it contains the data and log files, and all the schemas within it.
+   A Database is the collection of data i.e the main container, it contains the data and log files, and all the schemas within it.
    You always back up a database, it is a discrete unit on its own.
+
+   Database Management System:
+   A database management system is a collection of programs written to manage a database.
+   That is, it acts as a interface between user and database.
+   
+   Relational Database Management System:
+   It was developed by Dr. E.F. CODD and according to him any package can be called as RDBMS only if it satisfies the Codd Rules.
+   Link: "http://www.oracle-dba-online.com/sql/Codd_rules.htm" without the quote.
 
    Schemas are like folders within a database, and are mainly used to group logical objects together,
    which leads to ease of setting permissions by schema.
