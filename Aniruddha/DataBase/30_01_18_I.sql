735c02700b7fc5bf75edace5fca0107073fecac5
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-30 22:52:14 +0530

    Update 30_01_18_I.sql

--- Aniruddha/DataBase/30_01_18_I.sql
+++ Aniruddha/DataBase/30_01_18_I.sql
@@ -1,1 +1,8 @@
+/* 
+   A database is the main container, it contains the data and log files, and all the schemas within it.
+   You always back up a database, it is a discrete unit on its own.
 
+   Schemas are like folders within a database, and are mainly used to group logical objects together,
+   which leads to ease of setting permissions by schema.
+   All employee-related objects in the "employee" schema, etc.
+*/
