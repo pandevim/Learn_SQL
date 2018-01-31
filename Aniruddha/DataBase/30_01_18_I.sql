cb034628d42371682cdbe9a03c9fecf3fff9d24b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-31 14:54:11 +0530

    Update 30_01_18_I.sql

--- Aniruddha/DataBase/30_01_18_I.sql
+++ Aniruddha/DataBase/30_01_18_I.sql
@@ -1,13 +1,17 @@
 /* 
-   A Database is the collection of data i.e the main container, it contains the data and log files, and all the schemas within it.
+   A Database is the collection of data i.e the main container,
+   it contains the data and log files, and all the schemas within it.
    You always back up a database, it is a discrete unit on its own.
 
    Database Management System:
    A database management system is a collection of programs written to manage a database.
    That is, it acts as a interface between user and database.
+   Major (DBMS) are: Hierarchical, Network, Relational, Object-Oriented.
    
    Relational Database Management System:
-   Its model is developed by Dr. E.F. CODD and according to him any package can be called as RDBMS only if it satisfies the Codd Rules.
+   It is the most popular (DBMS).
+   Its model is developed by Dr. E.F. CODD and according to him
+   any package can be called as RDBMS only if it satisfies the Codd Rules.
    Link: "http://www.oracle-dba-online.com/sql/Codd_rules.htm" without the quote.
    Popular (RDBMS) are: Oracle, MySQL, PostgreSQL etc.
 
