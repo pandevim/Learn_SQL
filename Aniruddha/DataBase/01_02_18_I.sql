ba7e19cbc93f99b16716c41b3428b42eed6cf263
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-01 00:41:13 +0530

    Update 01_02_18_I.sql

--- Aniruddha/DataBase/01_02_18_I.sql
+++ Aniruddha/DataBase/01_02_18_I.sql
@@ -8,3 +8,18 @@ CREATE DATABASE DATABASE_NAME;
 SELECT
    *
 FROM ALL_USERS;
+
+/* 
+   To view information about the database from the control file.
+   The below query should used.
+   And to view information about instances,
+   the last query should be used.
+*/
+
+SELECT
+   *
+FROM v$database;
+
+SELECT
+   *
+FROM v$instance;
