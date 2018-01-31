729dd1699d2dfd6b4c3858a3fdfcb06845044cbe
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-31 20:27:35 +0530

    Update 31_01_18_II.sql

--- Aniruddha/DataBase/31_01_18_II.sql
+++ Aniruddha/DataBase/31_01_18_II.sql
@@ -11,4 +11,9 @@
    You can create a database at installation time, using the Database Configuration Assistant,
    or you can supply your own scripts for creating a database.
    (DBCA) is a Java based GUI tool which is very useful to create, configure and drop databases.
+   
+   When you start up a database, you create an instance of that database,
+   and you choose the state in which the database starts.
+   Normally, you would start up an instance by mounting and opening the database,
+   thus making it available for any valid user to connect to and perform typical data access operations.
 */
