1d3cc2500c5a4afe54456db7f38c10b637fbb6b6
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-31 14:02:37 +0530

    Create 31_01_18_I.sql

---
+++ Aniruddha/DataBase/31_01_18_I.sql
@@ -0,0 +1,5 @@
+/* 
+   Oracle Database contains Logical and Physical Structures.
+   Logical Structures are tablespaces, Schema objects, extents and segments.
+   Physical Structures are Datafiles, Redo Log Files, Control Files.
+*/
