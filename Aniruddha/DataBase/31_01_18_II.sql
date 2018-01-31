20d49589f5060aad94e2aab37c414076c57cbb4f
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-31 20:09:13 +0530

    Update 31_01_18_II.sql

--- Aniruddha/DataBase/31_01_18_II.sql
+++ Aniruddha/DataBase/31_01_18_II.sql
@@ -2,4 +2,13 @@
    Now the below work will required you to be the "Oracle Database Administrator"
    or you ought to have admin privileges.
    
+   As the database administrator, you must plan:
+   1.The logical storage structure of the database
+   2.The overall database design
+   3.A backup strategy for the database
+   
+   When you complete the database design, you can create the database and open it for normal use.
+   You can create a database at installation time, using the Database Configuration Assistant,
+   or you can supply your own scripts for creating a database.
+   (DBCA) is a Java based GUI tool which is very useful to create, configure and drop databases.
 */
