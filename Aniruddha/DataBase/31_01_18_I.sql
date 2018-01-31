8b149fcb38ca61600ff77395ac2a5ac67f760143
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-31 14:49:59 +0530

    Update 31_01_18_I.sql

--- Aniruddha/DataBase/31_01_18_I.sql
+++ Aniruddha/DataBase/31_01_18_I.sql
@@ -2,4 +2,7 @@
    Oracle Database contains Logical and Physical Structures.
    Logical Structures are tablespaces, Schema objects, extents and segments.
    Physical Structures are Datafiles, Redo Log Files, Control Files.
+   
+   All the tables and other objects in Oracle are stored in tablespace logically,
+   but physically they are stored in the datafiles associated with the tablespace.
 */
