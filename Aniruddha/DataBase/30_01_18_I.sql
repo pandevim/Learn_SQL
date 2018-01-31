798dadff3401048854629e42e3120c942012ef06
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-01-31 13:51:46 +0530

    Update 30_01_18_I.sql

--- Aniruddha/DataBase/30_01_18_I.sql
+++ Aniruddha/DataBase/30_01_18_I.sql
@@ -7,8 +7,9 @@
    That is, it acts as a interface between user and database.
    
    Relational Database Management System:
-   It was developed by Dr. E.F. CODD and according to him any package can be called as RDBMS only if it satisfies the Codd Rules.
+   Its model is developed by Dr. E.F. CODD and according to him any package can be called as RDBMS only if it satisfies the Codd Rules.
    Link: "http://www.oracle-dba-online.com/sql/Codd_rules.htm" without the quote.
+   Popular (RDBMS) are: Oracle, MySQL, PostgreSQL etc.
 
    Schemas are like folders within a database, and are mainly used to group logical objects together,
    which leads to ease of setting permissions by schema.
