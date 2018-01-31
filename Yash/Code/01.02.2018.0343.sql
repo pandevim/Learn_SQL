29a884ec7bc2fefe509f35a0307bcfc330fac177
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-01 03:49:20 +0530

    INSERT INTO

--- Yash/Code/01.02.2018.0343.sql
+++ Yash/Code/01.02.2018.0343.sql
@@ -1,1 +1,9 @@
+/*The INSERT INTO is used to insert a new data or record in the database*/
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');/*This will insert another record in the database as number 92. You don't have to add the number for number 92 as it is an auto-increment field*/
+
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Cardinal', 'Stavanger', 'Norway');/*This will do the same as above. But notice that there is no values assigend for few terms. So the database will display "null" in place of those non-assigned value fields.*/
+
 
