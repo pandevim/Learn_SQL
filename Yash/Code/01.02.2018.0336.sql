22ed8f96d8a6b1de7ce79a6753a90a7490cf3955
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-01 03:42:40 +0530

    ORDER BY

--- Yash/Code/01.02.2018.0336.sql
+++ Yash/Code/01.02.2018.0336.sql
@@ -1,1 +1,16 @@
+/*The ORDER BY statement is used for ordering the data accordint to the desired property.*/
+
+SELECT * FROM Customers
+ORDER BY Country;/*This will arrange the data in a way in which the countries name beginning with A will appear first then like always in alphabetical order*/
+
+SELECT * FROM Customers
+ORDER BY Country DESC;/*This will simply reverse the above operation. Above one by default was in the ascending order.*/
+
+SELECT * FROM Customers
+ORDER BY Country, CustomerName;/*This will first arrange the data on the basis of country and then again sort them according to the CustomerName.*/
+
+/*You can even change the order of different columns in the database*/
+SELECT * FROM Customers
+ORDER BY Country ASC, CustomerName DESC;/*This will arrange the country data in the ascending order whereas the CutomerName will be arranged in the descending order which leads to two layer filtration.*/
+
 
