df041c9813fff298176178303801e7b3f012b9c1
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-01 00:37:33 +0530

    Create 01_02_18_I.sql

---
+++ Aniruddha/DataBase/01_02_18_I.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE DATABASE_NAME;
+
+/* 
+   The above query will create a new database with name "DATABASE_NAME".
+   To view all "Schemas" the below query should used.
+*/
+
+SELECT
+   *
+FROM ALL_USERS;
