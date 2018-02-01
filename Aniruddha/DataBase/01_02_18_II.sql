40fc8e0a06796519d75f5cc902a7dc0e56ae13be
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-01 15:50:26 +0530

    Update 01_02_18_II.sql

--- Aniruddha/DataBase/01_02_18_II.sql
+++ Aniruddha/DataBase/01_02_18_II.sql
@@ -2,11 +2,12 @@ CREATE TABLE TABLE_NAME(
    COLUMN_1 DATATYPE NOT NULL,
    COLUMN_2 DATATYPE(WIDTH),
    COLUMN_3 DATATYPE
-   CONSTRAINT pk_TABLE_NAME PRIMARY KEY (TABLE_NAME_NO)
+   CONSTRAINT pk_TABLE_NAME PRIMARY KEY (PRIMARY_COLUMN_1)
 );
 
 /* 
    This is the syntax of creating a table.
+   Now, this table can be filled using "INSERT INTO" clause.
    Here the "WIDTH" defines the maximum quantity of lenght it can hold.
    Or otherwise, the width can be predetermined by the datatype it self, as in "DATE" datatype.
    If columns are of the NUMBER datatype, define precision and scale instead of width.
@@ -25,4 +26,23 @@ CREATE TABLE TABLE_NAME AS(
 /* 
    We can also create table using another table like mentioned above.
    This table will be the exact copy of table "Customers".
+*/
+
+CREATE TABLE TABLE_NAME(
+   COLUMN_1 DATATYPE,
+   COLUMN_2 DATATYPE,
+   COLUMN_3 DATATYPE
+   CONSTRAINT pk_TABLE_NAME PRIMARY KEY (PRIMARY_COLUMN_1)
+   CONSTRAINT fk_TABLE_NAME FOREIGN KEY (PRIMARY_COLUMN_2)
+   REFERENCE Customers (PRIMARY_COLUMN_2)
+);
+
+/* 
+   The "REFERENCE" keyword is also known as "Referential Integrity".
+   As tables can declarative specify realtionships between tables.
+   Here "pk" and "fk" are "PRIMARY KEY" and "FOREIGN KEY".
+   The above query will create a "child" table, name "TABLE_NAME" of the table
+   "Customers" by including a foreigh key in the "TABLE_NAME" table that references
+   the "Customers" table.
+   "FOREIGN KEY" must reference "PRIMARY KEY" to make "parent" "child" relation in tables.
 */
