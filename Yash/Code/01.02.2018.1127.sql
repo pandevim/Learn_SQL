b0b783cd5baf1d9f3c856914b32135a32d8cd90e
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-01 11:31:30 +0530

    COUNT AVG SUM

--- Yash/Code/01.02.2018.1127.sql
+++ Yash/Code/01.02.2018.1127.sql
@@ -1,1 +1,12 @@
+/*COUNT, AVG and SUM functions*/
 
+SELECT COUNT(ProductID)
+FROM Products;/*This will show the count of records in the ProductID column. In this case its 77*/
+
+SELECT AVG(Price)
+FROM Products;/*This will tell you the Average of the Price column data.*/
+
+SELECT SUM(Quantity)
+FROM OrderDetails;/*This will sum up all of the data in the Quantity column*/
+
+/*These all are so handy when working on extracting statistics from the database*/
