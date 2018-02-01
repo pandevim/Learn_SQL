1b8e951cea303e72e5ccf4b2208140ee788415eb
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-01 11:07:02 +0530

    DELETE

--- Yash/Code/01.02.2018.1102.sql
+++ Yash/Code/01.02.2018.1102.sql
@@ -1,1 +1,7 @@
-SELECT 
+/*The DELETE statement*/
+/*This statement is used to delete an existing record from the database*/
+
+DELETE FROM Customers
+WHERE CustomerName='Alfreds Futterkiste';/*This will delete the records with CustomerName as Alfreds Futterkiste from the database. Hence, we have 90 records.*/
+
+
