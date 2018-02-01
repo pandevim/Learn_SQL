c0b8656dbfe11424723a73fde823a2c44c4d808e
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-01 11:38:04 +0530

    LIKE

--- Yash/Code/01.02.2018.1131.sql
+++ Yash/Code/01.02.2018.1131.sql
@@ -1,1 +1,32 @@
+/*
 
+WHERE CustomerName LIKE 'a%'	Finds any values that starts with "a"
+WHERE CustomerName LIKE '%a'	Finds any values that ends with "a"
+WHERE CustomerName LIKE '%or%'	Finds any values that have "or" in any position
+WHERE CustomerName LIKE '_r%'	Finds any values that have "r" in the second position
+WHERE CustomerName LIKE 'a_%_%'	Finds any values that starts with "a" and are at least 3 characters in length
+WHERE ContactName LIKE 'a%o'	Finds any values that starts with "a" and ends with "o"
+
+Source:W3Schools
+*/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a%';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%a';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%or%'
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '_r%';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a_%_%';
+
+SELECT * FROM Customers
+WHERE ContactName LIKE 'a%o';
+
+SELECT * FROM Customers
+WHERE CustomerName NOT LIKE 'a%';
