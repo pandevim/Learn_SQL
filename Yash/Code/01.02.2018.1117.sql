1d028bbc3b8de91478c468de327d9a0df4fb43cf
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-01 11:21:59 +0530

    MIN MAX

--- Yash/Code/01.02.2018.1117.sql
+++ Yash/Code/01.02.2018.1117.sql
@@ -1,1 +1,10 @@
+/*MIN and MAX*/
+/*MIN and MAX statements are used for the identification of the smallest or biggest value from a table's column or record*/
+
+SELECT MIN(Price) AS SmallestPrice
+FROM Products;/*This will show you the miinimum valur from the price column and will print as SmallestPrice. Notice the use of AS statement.*/
+
+SELECT MAX(Price) AS LargestPrice
+FROM Products;/*Similar to above, just displays Largestprice*/
+
 
