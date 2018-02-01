484cafa5e5ba3bba18285b9f8a2b6dafef4f7fe7
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-01 14:44:18 +0530

    IN

--- Yash/Code/01.02.2018.1431.sql
+++ Yash/Code/01.02.2018.1431.sql
@@ -1,1 +1,12 @@
+/*IN*/
+/*The IN statement shows the files with a required condition fulfilled among two or more different things/prperties.*/
+
+SELECT * FROM Customers
+WHERE Country IN ('Germany', 'France', 'UK');/*This will display the list of records which have UK, France and Germany*/
+
+SELECT * FROM Customers
+WHERE Country NOT IN ('Germany', 'France', 'UK');/*This will select from the list which do not contain anything from the three countries*/
+
+SELECT * FROM Customers
+WHERE Country IN (SELECT Country FROM Suppliers);/*This will show all the countries in another database. In this case we are calling Suppliers database's countries from the Customers database. This is where this statement comes in hany.*/
 
