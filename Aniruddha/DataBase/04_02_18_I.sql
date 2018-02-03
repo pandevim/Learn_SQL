cd02e58915195f11607e6207e5f52907258f5e8d
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-04 01:28:49 +0530

    Update 04_02_18_I.sql

--- Aniruddha/DataBase/04_02_18_I.sql
+++ Aniruddha/DataBase/04_02_18_I.sql
@@ -4,3 +4,11 @@
    including indexes and triggers, you will get to know about these in further commits.
 */
 
+DROP TABLE TABLE_NAME CASCADE CONSTRAINTS;
+
+/* 
+   The optional "CASCADE CONSTRAINTS" clause will
+   drop remove all referencial integrityconstraints
+   that refer to the primary and unique keys thus
+   allowing you to drop database tables in any order.
+*/
