e2b75ec689f6e2477cb5c800e85c28bc257790f4
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-04 01:59:06 +0530

    Create 04_02_18_II.sql

---
+++ Aniruddha/DataBase/04_02_18_II.sql
@@ -0,0 +1,5 @@
+/* 
+   "Triggers" are procedures that are stored in the database and are implicitly run,
+   or fired, when something happens.
+   "Triggers" are supported by "Procedural Language SQL" or PL/SQL.
+*/
