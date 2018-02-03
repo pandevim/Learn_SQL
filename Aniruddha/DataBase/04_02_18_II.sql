ba0e13b0917bc10a2a44f984d90492377418ead1
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-04 02:08:18 +0530

    Update 04_02_18_II.sql

--- Aniruddha/DataBase/04_02_18_II.sql
+++ Aniruddha/DataBase/04_02_18_II.sql
@@ -1,5 +1,13 @@
 /* 
    "Triggers" are procedures that are stored in the database and are implicitly run,
-   or fired, when something happens.
+   or fired, when something happens or they are similar to stored procedures.
    "Triggers" are supported by "Procedural Language SQL" or PL/SQL.
+   "Triggers" are frequently used to automatically populate table "Primary Keys".
+   Like this example below from PL/SQL.
 */
+
+CREATE OR REPLACE TRIGGER TABLE_NAME_TRIGGER
+   BEFORE INSERT OR UPDATE ON TABLE_NAME
+   FOR EACH ROW
+BEGIN
+   IF "CONDITION"
