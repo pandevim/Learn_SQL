b9300f5bd103c325ff5774664a46f53add4dff95
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-04 01:38:07 +0530

    Update 04_02_18_I.sql

--- Aniruddha/DataBase/04_02_18_I.sql
+++ Aniruddha/DataBase/04_02_18_I.sql
@@ -12,3 +12,11 @@ DROP TABLE TABLE_NAME CASCADE CONSTRAINTS;
    that refer to the primary and unique keys thus
    allowing you to drop database tables in any order.
 */
+
+/* 
+   The "TRUNCATE TABLE" statement is used to delete the data (i.e all rows)
+   inside a table, but not the table itself.
+   But it is a DLL command and cannot be rolled back or retrieve through "FLASHBACK"
+   (i.e  non recoverable) and hence faster than "DELETE" command.
+*/
+
