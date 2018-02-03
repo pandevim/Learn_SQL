121b41d94de0714b1ae39b000deeab9049e97080
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-04 00:55:32 +0530

    Create 04_02_18_I.sql

---
+++ Aniruddha/DataBase/04_02_18_I.sql
@@ -0,0 +1,6 @@
+/* 
+   The "DROP TABLE" statement is used to drop an existing table in a database.
+   Dropping a table will remove all of the rows and drop sub-objects
+   including indexes and triggers, you will get to know about these in further commits.
+*/
+
