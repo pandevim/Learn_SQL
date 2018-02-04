f1ae25ef79346280ff83eb94171252226cc57d9d
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-05 03:46:19 +0530

    Update 05_02_18_I.sql

--- Aniruddha/DataBase/05_02_18_I.sql
+++ Aniruddha/DataBase/05_02_18_I.sql
@@ -1,4 +1,10 @@
 /* 
    Now that we have tables created, and we have triggers to automatically
    populate our primary keys, we can add data to our tables.
+   Because we have a parent child relationship, with the DEPARTMENTS table as the parent table,
+   and the EMPLOYEES table as the child we will first INSERT a row into the DEPARTMENTS table.
 */
+
+INSERT INTO
+   Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Aniruddha Pandey','Tody','LPU','Jalandar','144411','India');
