160f1ae3a28a154bdea1bb7fead339cf7f0a3633
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-05 07:06:57 +0530

    Update 05_02_18_II.sql

--- Aniruddha/DataBase/05_02_18_II.sql
+++ Aniruddha/DataBase/05_02_18_II.sql
@@ -3,4 +3,16 @@
    1.To enforce unique values within a column.
    2.To improve data access performance.
    3.To prevent lock escalation when updating rows of tables that use declarative referential integrity.
+   
+   When a table is created and a "PRIMARY KEY" is specified
+   an index is automatically created to enforce the primary key constraint.
+   To see the indexes that already exist a "Dictionary Query" can be used like this below.
 */
+
+SELECT
+   table_name 'Table', 
+   index_name 'Index', 
+   column_name 'Column', 
+   column_position 'Position'
+FROM  user_ind_columns  WHERE table_name='TABLE_NAME_1' or table_name='TABLE_NAME_2'
+ORDER BY table_name, column_name, column_position; --this line is optional
