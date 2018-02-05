cd5789181d96840c834552be44889653a6902b13
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-05 05:37:26 +0530

    Update 05_02_18_I.sql

--- Aniruddha/DataBase/05_02_18_I.sql
+++ Aniruddha/DataBase/05_02_18_I.sql
@@ -1,18 +1,18 @@
 /* 
    Now that we have tables created, and we have triggers to automatically
    populate our primary keys, we can add data to our tables.
-   Because we have a parent child relationship, with the "Customers" table as the parent table,
-   and the "TABLE_NAME" table as the child we will first "INSERT" a row into the "Customers" table.
+   Because we have a parent child relationship, with the "TABLE_NAME_1" table as the parent table,
+   and the "TABLE_NAME_2" table as the "child" we will first "INSERT" a row into the "TABLE_NAME_1" table.
    Or we can say we have used "referential integrity" which ensures that references
-   between data are indeed valid and intact, so we first need to firt fill the "Customers" table.
+   between data are indeed valid and intact, so we first need to firt fill the "TABLE_NAME_1" table.
 */
 
 INSERT INTO
-   Customers (CustomerName, Address, City, PostalCode, Country)
+   TABLE_NAME_1 (CustomerName, Address, City, PostalCode, Country)
 VALUES ('Tody','Lovely','Jalandar','144411','India');
 
 INSERT INTO
-   Customers (CustomerName, Address, City, PostalCode, Country)
+   TABLE_NAME_1 (CustomerName, Address, City, PostalCode, Country)
 VALUES ('Mody','Prestige','Indore','452010','India');
 
 /* 
@@ -21,16 +21,16 @@ VALUES ('Mody','Prestige','Indore','452010','India');
 */
 
 INSERT INTO
-   TABLE_NAME (Name, Job, Salary, CustomerID) 
-VALUES ('Aniruddha Pandey','Jobless', INR 0, (SELECT CustomerID FROM Customers WHERE Name='Tody'));
+   TABLE_NAME_2 (Name, Job, Salary, CustomerID) 
+VALUES ('Aniruddha Pandey','Jobless', INR 0, (SELECT CustomerID FROM TABLE_NAME_1 WHERE Name='Tody'));
 
 INSERT INTO
-   TABLE_NAME (Name, Job, Salary, CustomerID) 
-VALUES ('Ayush Pandey','Student', INR 0, (SELECT CustomerID FROM Customers WHERE Name='Mody'));
+   TABLE_NAME_2 (Name, Job, Salary, CustomerID) 
+VALUES ('Ayush Pandey','Student', INR 0, (SELECT CustomerID FROM TABLE_NAME_1 WHERE Name='Mody'));
 
 /* 
-   Two row of the table "TABLE_NAME" is filled.
-   This will generate random "TABLE_NAME_ID".
-   Since we had make a "parent" "child" relation in table "Customers" and "TABLE_NAME"
-   we need to put the "CustomerID" and join this coulumn with "TABLE_NAME_ID".
+   Two row of the table "TABLE_NAME_1" is filled.
+   This will generate random "TABLE_NAME_2_ID".
+   Since we had make a "parent" "child" relation in table "TABLE_NAME_1" and "TABLE_NAME_2"
+   we need to put the "TABLE_NAME_1_ID" and join this coulumn with "TABLE_NAME_2_ID".
 */
