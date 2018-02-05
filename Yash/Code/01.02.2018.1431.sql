a3b5f104e9f12be38979f9583001f2ba43516c58
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-05 17:24:55 +0530

    IN

--- Yash/Code/01.02.2018.1431.sql
+++ Yash/Code/01.02.2018.1431.sql
@@ -7,6 +7,6 @@ WHERE Country IN ('Germany', 'France', 'UK');/*This will display the list of rec
 SELECT * FROM Customers
 WHERE Country NOT IN ('Germany', 'France', 'UK');/*This will select from the list which do not contain anything from the three countries*/
 
-SELECT * FROM Customers
+SELECT *FROM Customers
 WHERE Country IN (SELECT Country FROM Suppliers);/*This will show all the countries in another database. In this case we are calling Suppliers database's countries from the Customers database. This is where this statement comes in handy.*/
 
