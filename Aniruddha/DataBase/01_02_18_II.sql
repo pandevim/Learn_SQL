6778cbcb0f92a5dd3fa1d4d459285d890148745a
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-05 05:30:09 +0530

    Update 01_02_18_II.sql

--- Aniruddha/DataBase/01_02_18_II.sql
+++ Aniruddha/DataBase/01_02_18_II.sql
@@ -1,4 +1,4 @@
-CREATE TABLE TABLE_NAME(
+CREATE TABLE TABLE_NAME_1(
    COLUMN_1 DATATYPE NOT NULL PRIMARY KEY,
    COLUMN_2 DATATYPE(WIDTH),
    COLUMN_3 DATATYPE NOT NULL
@@ -6,7 +6,7 @@ CREATE TABLE TABLE_NAME(
 
 --or
 
-CREATE TABLE TABLE_NAME(
+CREATE TABLE TABLE_NAME_1(
    COLUMN_1 DATATYPE,
    COLUMN_2 DATATYPE(WIDTH),
    COLUMN_3 DATATYPE
@@ -30,26 +30,27 @@ CREATE TABLE TABLE_NAME(
    Here there is only "PRIMARY KEY" which is "pk_TABLE_NAME" but its value is (COLUMN_1+COLUMN_3).
 */
 
-CREATE TABLE TABLE_NAME AS(SELECT * FROM Customers);
+CREATE TABLE TABLE_NAME_1 AS(SELECT * FROM Customers);
 
 /* 
    We can also create table using another table like mentioned above.
    This table will be the exact copy of table "Customers".
 */
 
-CREATE TABLE TABLE_NAME(
+CREATE TABLE TABLE_NAME_2(
    COLUMN_1 DATATYPE,
    COLUMN_2 DATATYPE,
    COLUMN_3 DATATYPE
-   CONSTRAINT pk_TABLE_NAME PRIMARY KEY (COLUMN_1)
+   CONSTRAINT pk_TABLE_NAME_1 PRIMARY KEY (COLUMN_1)
    CONSTRAINT fk_TABLE_NAME FOREIGN KEY (PRIMARY_COLUMN_2) REFERENCES Customers (PRIMARY_COLUMN_2)
 );
 
 /* 
    The "REFERENCE" keyword is also known as "Referential Integrity".
    As tables can declarative specify realtionships between tables.
-   The above query will create a "child" table, name "TABLE_NAME" of the table
-   "Customers" by including a "FOREIGN KEY" in the "TABLE_NAME" table that references
-   the "Customers" table.
+   A "FOREIGN KEY" is a key used to link two tables together.
+   It is a field (or collection of fields) in one table that refers to the PRIMARY KEY in another table.
+   The above query will create a "child" table, name "TABLE_NAME_1" of the table
+   "TABLE_NAME_2" by including a "FOREIGN KEY" in the "TABLE_NAME_2" table that references the "TABLE_NAME_1" table.
    "FOREIGN KEY" must reference "PRIMARY KEY" to make "parent" "child" relation in tables.
 */
