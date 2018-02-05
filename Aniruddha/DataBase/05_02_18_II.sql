8ba760d6f737e002fd0c5c1dd3ba1c002e7c7619
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-05 07:00:16 +0530

    Update 05_02_18_II.sql

--- Aniruddha/DataBase/05_02_18_II.sql
+++ Aniruddha/DataBase/05_02_18_II.sql
@@ -1,1 +1,6 @@
-
+/* 
+   Typically developers index columns for three major reasons:
+   1.To enforce unique values within a column.
+   2.To improve data access performance.
+   3.To prevent lock escalation when updating rows of tables that use declarative referential integrity.
+*/
