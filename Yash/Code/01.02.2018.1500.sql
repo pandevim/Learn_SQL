723ac911b4944ac9fae97975f911cbda2c067795
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-06 13:47:55 +0530

    ALIASES

--- Yash/Code/01.02.2018.1500.sql
+++ Yash/Code/01.02.2018.1500.sql
@@ -1,1 +1,13 @@
+/*Aliases*/
+-- Used to give a temporary name to a table. Normally used when the names are user oriented and not public oriented. 
+
+SELECT CustomerID as ID, CustomerName AS Customer
+FROM Customers; -- Shows the CustomerID name changed to ID
+
+SELECT CustomerName, Address + ', ' + PostalCode + ' ' + City + ', ' + Country AS Address
+FROM Customers;-- This will join different columns records to show the output as address of a customer.
+
+SELECT CustomerName, CONCAT(Address,', ',PostalCode,', ',City,', ',Country) AS Address
+FROM Customers;--For MySQL
+
 
