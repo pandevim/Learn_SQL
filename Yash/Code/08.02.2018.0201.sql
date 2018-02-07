b7825fb3f0b78e78837e23583ad255e9ae5dc0f4
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-08 02:32:31 +0530

    HAVING

--- Yash/Code/08.02.2018.0201.sql
+++ Yash/Code/08.02.2018.0201.sql
@@ -1,1 +1,10 @@
+--HAVING
+--It's llke an advanced and extended form of the WHERE clause.
 
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+WHERE LastName = 'Davolio' OR LastName = 'Fuller'
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 25;
+--Shows the output with the OrderID with COUNT greater than 25.
