f5f8119da4753cb897f5eaa0bff9b40334ce4277
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-08 02:44:15 +0530

    EXISTS

--- Yash/Code/02.08.2018.0239.sql
+++ Yash/Code/02.08.2018.0239.sql
@@ -1,1 +1,8 @@
+--EXISTS
+--Used to test for the existence of any data in the table.
 
+SELECT SupplierName
+FROM Suppliers
+WHERE EXISTS (SELECT ProductName FROM Products WHERE SupplierId = Suppliers.supplierId AND Price < 20);
+
+--This will first go to Suppliers and then select the files whch staisfy to fulfil the ProductName, Products and Price less than 20
