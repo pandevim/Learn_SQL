49c9f76a78e4cc34fe5afc2c49b9e7eca1aed39b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-07 11:19:14 +0530

    Update 07_02_18_I.sql

--- Aniruddha/DataBase/07_02_18_I.sql
+++ Aniruddha/DataBase/07_02_18_I.sql
@@ -1,4 +1,10 @@
 /* 
    The "ALTER TABLE" clause can add, delecte and modify additional columns in an existing table.
-   
+*/
+
+ALTER TABLE TABLE_NAME_2 
+ADD COLUMN_2 DATATYPE(WIDTH);
+
+/* 
+   The above query will add a column to the right side of the table.
 */
