007411c049f8bfb63f867a4df433a888689225f3
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-08 01:51:50 +0530

    UNION

--- Yash/Code/08.02.2018.0150.sql
+++ Yash/Code/08.02.2018.0150.sql
@@ -1,1 +1,8 @@
+--UNION 
+--Used to combine two or more SELECT statements.
+SELECT City FROM Customers
+UNION
+SELECT City FROM Suppliers
+ORDER BY City;
 
+--Combines the cities from both of the tables into a single. 
