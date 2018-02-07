c2e9580b175d4c69b5c23b53804365997918dcb3
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-08 01:59:37 +0530

    GROUP BY

--- Yash/Code/08.02.2018.0153.sql
+++ Yash/Code/08.02.2018.0153.sql
@@ -1,1 +1,12 @@
+--GROUP BY
+--This is used as the names suggestes for groping. The data can be arranged and group as the statements are passed. 
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+ORDER BY COUNT(CustomerID) DESC;
+--This first passes COUNT for CustomerID and Country then arranges as per the priority for the coutries. Also arranges in DESC statement. 
 
+SELECT Shippers.ShipperName,COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
+LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
+GROUP BY ShipperName;
+--This selects from oders and counts the number of orders them the IDs and so on. Also then all of the data is arranged and grouped. 
