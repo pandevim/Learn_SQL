2a00967e45864cf1ecd8af7aeffd965ee9c653b2
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-08 22:51:48 +0530

    Update 07_02_18_II.sql

--- Aniruddha/DataBase/07_02_18_II.sql
+++ Aniruddha/DataBase/07_02_18_II.sql
@@ -1,3 +1,14 @@
 /* 
    "Data Dictonary" is a read-only set of tables that provides information about the database.
+   It contains:
+   
+   1)The definitions of all schema objects in the database:
+      (tables, views, indexes, clusters, synonyms, sequences, procedures, functions, packages, triggers, and so on).
+   2)How much space has been allocated for, and is currently used by, the schema objects.
+   3)Default values for columns.
+   4)Integrity constraint information.
+   5)The names of Oracle users.
+   6)Privileges and roles each user has been granted.
+   7)Auditing information, such as who has accessed or updated various schema objects.
+   8)Other general database information like meta data.
 */
