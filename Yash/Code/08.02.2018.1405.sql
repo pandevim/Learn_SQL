7a6f1118f46b41f3840c0efbfacae8fff445ab97
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-08 14:20:30 +0530

    INSERT INTO SELECT and Comments

--- Yash/Code/08.02.2018.1405.sql
+++ Yash/Code/08.02.2018.1405.sql
@@ -7,3 +7,6 @@
 INSERT INTO Customers (CustomerName, City, Country)
 SELECT SupplierName, City, Country FROM Suppliers
 WHERE Country='Germany'; --This changes all the applied conditions for the statement to be true.
+
+--Moreover we have been using Comments already. Still we have two ways to declare comments. First just like this - using 2 hyphens
+/*Second - in this manner*/
