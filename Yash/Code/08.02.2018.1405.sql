972a9a1602070c6efa5e64364dce0c5becfe07d1
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-08 14:07:27 +0530

    INSERT INTO SELECT

--- Yash/Code/08.02.2018.1405.sql
+++ Yash/Code/08.02.2018.1405.sql
@@ -1,1 +1,9 @@
+--INSERT INTO SELECT
+--This statement copies data from one table and inserts it into another table.
+--It requires that data types in source and target tables match
+--The existing records in the target table are unaffected
 
+
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers
+WHERE Country='Germany'; --This changes all the applied conditions for the statement to be true.
