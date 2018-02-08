36653f3abc6aff320632a74c77ddef1dfe250316
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-08 14:04:08 +0530

    SELECT INTO

--- Yash/Code/08.02.2018.1400.sql
+++ Yash/Code/08.02.2018.1400.sql
@@ -1,1 +1,13 @@
+--SELECT INTO
+--The SELECT INTO statement copies data from one table into a new table.
+--Can also be used to create a new, empty table using the schema of another.
+
+SELECT * INTO CustomersGermany
+FROM Customers
+WHERE Country = 'Germany';
+
+SELECT * INTO CustomersBackup2017 IN 'Backup.mdb'
+FROM Customers;
+--The following SQL statement uses the IN clause to copy the table into a new table in another database.
+--.mdb extension file is created to showcase the obtained database.
 
