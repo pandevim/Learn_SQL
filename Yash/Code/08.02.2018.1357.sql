0a01c8acb8281f5f4fc055e341e8a1a30a6063fb
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-08 14:00:09 +0530

    ANY, ALL

--- Yash/Code/08.02.2018.1357.sql
+++ Yash/Code/08.02.2018.1357.sql
@@ -1,1 +1,11 @@
+--ANY and ALL Operators
+/*The ANY and ALL operators are used with a WHERE or HAVING clause.
+
+The ANY operator returns true if any of the subquery values meet the condition.
+
+The ALL operator returns true if all of the subquery values meet the condition.*/
+
+SELECT ProductName
+FROM Products
+WHERE ProductID = ANY (SELECT ProductID FROM OrderDetails WHERE Quantity > 99);--Moreover basic operators are used: + - / * %
 
