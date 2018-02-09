a92c44d14933f4bc9f16e7da0f68fac9ffa9cfe1
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-10 00:52:16 +0530

    README

--- Aniruddha/29_11_17.sql
+++ Aniruddha/29_11_17.sql
@@ -3,7 +3,7 @@
    Oracle, Microsoft and MySQL are some of the examples of languages of SQL.
    like PL/SQL, JET SQL, T -SQL and respectively.
 */
-
+ 
 --A primary key is a field in the table that uniquely identifies the table records which can't be NULL.
 --You can see the CustomerID section, database provided in "README.md".
 --Here we will learn some common commands which will be applicable in all.
