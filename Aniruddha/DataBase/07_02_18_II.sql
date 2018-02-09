e11b4f1beddd16db8aaf55a7ca7c2d62ba9afe2b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-10 00:00:29 +0530

    Update 07_02_18_II.sql

--- Aniruddha/DataBase/07_02_18_II.sql
+++ Aniruddha/DataBase/07_02_18_II.sql
@@ -2,8 +2,8 @@
    "Data Dictonary" is a read-only set of tables that provides information about the database.
    It contains:
    
-   1)The definitions of all schema objects in the database:
-      (tables, views, indexes, clusters, synonyms, sequences, procedures, functions, packages, triggers, and so on).
+   1)The definitions of all schema objects in the database i.e:
+    (tables, views, indexes, clusters, synonyms, sequences, procedures, functions, packages, triggers, and so on).
    2)How much space has been allocated for, and is currently used by, the schema objects.
    3)Default values for columns.
    4)Integrity constraint information.
@@ -14,3 +14,7 @@
    
    All the data dictionary tables and views for a given database are stored in that database's "SYSTEM" tablespace.
 */
+
+SELECT TABLE_NAME, TABLESPACE_NAME, STATUS
+FROM TABLE_NAME_1
+where TABLE_NAME='TABLE_NAME_1';
