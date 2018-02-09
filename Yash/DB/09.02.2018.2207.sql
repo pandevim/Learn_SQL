8c67c18e23e8f65ad1739094d427608b1a76f0b9
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-09 22:11:51 +0530

    DROP TRUNCATE TABLE

--- Yash/DB/09.02.2018.2207.sql
+++ Yash/DB/09.02.2018.2207.sql
@@ -1,1 +1,9 @@
+--DROP TABLE
+--This is used to delete a table from the databse.
 
+DROP TABLE Shippers; --This deletes the table named Shippers from the database.
+
+--TRUNCATE TABLE
+--This is used to delete the data inside the table and not the table itself.
+
+TRUNCATE TABLE Shippers;--This deletes the data from the table named Shippers and not the table itself.
