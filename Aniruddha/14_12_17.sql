534190d8d54f817e7474351cd3a221b4c0fddbc1
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-10 00:06:41 +0530

    SELECT

--- Aniruddha/14_12_17.sql
+++ Aniruddha/14_12_17.sql
@@ -2,7 +2,7 @@
    All queries are done on the database objects called "tables".
    These tables are made up of "fields".
 */
-
+ 
 SELECT
    *
 FROM Customers; --This SQL statement selects all the records from the table name "Customers".
