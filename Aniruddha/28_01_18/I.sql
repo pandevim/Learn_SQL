b18922cda6e6c3395db0b997e0833098bf1306fd
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-10 00:03:37 +0530

    INNER JOIN

--- Aniruddha/28_01_18/I.sql
+++ Aniruddha/28_01_18/I.sql
@@ -2,7 +2,7 @@
    The "JOIN" clause is used to combine rows from two or more tables,
    based on a related column between them.
 */
-
+ 
 SELECT
    *
 FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
