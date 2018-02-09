ea93abbfc5506494aa185ae5df9834b1e776f6ba
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-10 00:17:59 +0530

    NOT NULL

--- Yash/DB/09.02.2018.2247.sql
+++ Yash/DB/09.02.2018.2247.sql
@@ -1,1 +1,11 @@
+--NOT NULL
+
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255) NOT NULL,
+    Age int
+);
+
+--Explained already in the previous
 
