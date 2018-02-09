cf78c2b5329ba7c3dbee42e2b5f80580a1b300da
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-09 22:06:52 +0530

    CREATE TABLE

--- Yash/DB/09.02.2018.2204.sql
+++ Yash/DB/09.02.2018.2204.sql
@@ -1,1 +1,13 @@
+--CREATE TABLE
 
+--This statement is used to create table. Same for most of the query languages
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255) 
+);
+
+--This makes the table with the name Persons. And the LastName and Adress etc. as the title for the column title.
