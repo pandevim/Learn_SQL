258a5fd6d0d1b8d00e38906f10b112a0a41cee9a
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-09 22:42:40 +0530

    Constraints

--- Yash/DB/09.02.2018.2240.sql
+++ Yash/DB/09.02.2018.2240.sql
@@ -1,1 +1,18 @@
+--Constraints 
 
+CREATE TABLE table_name (
+    column1 datatype constraint,
+    column2 datatype constraint,
+    column3 datatype constraint,
+    ....
+);
+
+/*
+NOT NULL - Ensures that a column cannot have a NULL value
+UNIQUE - Ensures that all values in a column are different
+PRIMARY KEY - A combination of a NOT NULL and UNIQUE. Uniquely identifies each row in a table
+FOREIGN KEY - Uniquely identifies a row/record in another table
+CHECK - Ensures that all values in a column satisfies a specific condition
+DEFAULT - Sets a default value for a column when no value is specified
+INDEX - Used to create and retrieve data from the database very quickly
+*/
