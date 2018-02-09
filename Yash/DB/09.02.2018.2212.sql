b907594488a1ba84e5e3866c604406e473660907
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-09 22:15:03 +0530

    ALTER

--- Yash/DB/09.02.2018.2212.sql
+++ Yash/DB/09.02.2018.2212.sql
@@ -1,1 +1,12 @@
+--The ALTER TABLE statement is used to add, delete, or modify columns in an existing table.
+
+ALTER TABLE Persons
+ADD DateOfBirth date;
+
+ALTER TABLE Persons
+ALTER COLUMN DateOfBirth year;
+
+ALTER TABLE Persons
+DROP COLUMN DateOfBirth;
+
 
