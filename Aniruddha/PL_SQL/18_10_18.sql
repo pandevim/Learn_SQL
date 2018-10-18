abdac1e773115884786e467931b4da92a9c64a65
Aniruddha Pandey <anirudh.pandev@gmail.com>
2018-10-18 09:42:03 +0530

    Update 18_10_18.sql

--- Aniruddha/PL_SQL/18_10_18.sql
+++ Aniruddha/PL_SQL/18_10_18.sql
@@ -1,4 +1,26 @@
+/*
 -- variable_name CONSTANT datatype(size) := number
 -- i.e
 
 PI CONSTANT double(30) := 3.14
+
+-- Display Output
+
+dbms_output.put_line('Hello, World!')
+
+Different literal in PL/SQL:
+
+1) Numeric
+2) Character
+3) String
+4) Boolean
+5) Date & Time
+
+Arthmetic Operators:
+1) Greater than equal to
+
+Comparasion Operators:
+
+Logical Opertors:
+
+*/
